5a6babbde914fe71c6063d6080eda79ba941c313
Alecsandru Dinu <dinu@alecsandru.ro>
2017-06-27 07:52:08 +0300

    Fixed empty file error

---
+++ Api/GuestTwispayPaymentDetailsInterface.php
@@ -0,0 +1,20 @@
+<?php
+namespace Twispay\Payments\Api;
+
+/**
+ * Interface GuestTwispayPaymentDetailsInterface
+ * @api
+ * @package Twispay\Payments\Api
+ * @author Webliant Software
+ */
+interface GuestTwispayPaymentDetailsInterface
+{
+
+	/**
+	 * @param string $orderId
+	 *
+	 * @return \Twispay\Payments\Service\V1\Data\OrderPaymentResponse
+	 */
+	public function getPaymentDetails($orderId);
+
+}
