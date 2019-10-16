e3d03d14f54ff2417123af4d7a9728ba4d4deac5
cosmin_bosutar <bosutar.cosmin@gmail.com>
2019-10-16 12:05:15 +0300

    Added CSRF validation hook to ensure backward compatibility.

---
+++ Plugin/CsrfValidatorSkip.php
@@ -0,0 +1,20 @@
+<?php
+namespace Twispay\Payments\Plugin;
+
+class CsrfValidatorSkip {
+  /**
+   * @param \Magento\Framework\App\Request\CsrfValidator $subject
+   * @param \Closure $proceed
+   * @param \Magento\Framework\App\RequestInterface $request
+   * @param \Magento\Framework\App\ActionInterface $action
+   */
+  public function aroundValidate($subject, \Closure $proceed, $request, $action) {
+    if ($request->getModuleName() == 'twispay') {
+      /* Skip CSRF check. */
+      return;
+    } else {
+      /* Proceed Magento 2 core functionalities. */
+      $proceed($request, $action);
+    }
+  }
+}
