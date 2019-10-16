6af2933b544ce10ab1ad18cb237ed957e03c93f3
cosmin_bosutar <bosutar.cosmin@gmail.com>
2019-10-16 12:49:57 +0300

    Added support for refund and partial refund

--- Model/Twispay.php
+++ Model/Twispay.php
@@ -3,6 +3,7 @@
 namespace Twispay\Payments\Model;
 
 use Magento\Framework\Exception\PaymentException;
+use Magento\Sales\Model\Order\Payment\Transaction;
 
 /**
  * Twispay payment method model
@@ -15,12 +16,12 @@ use Magento\Framework\Exception\PaymentException;
 class Twispay extends \Magento\Payment\Model\Method\AbstractMethod {
   const METHOD_CODE = 'twispay';
 
+  /** @var \Twispay\Payments\Helper\Payment */
+  private $helper;
   /** @var \Twispay\Payments\Logger\Logger */
   private $log;
-  /** @var \Magento\Framework\UrlInterface */
-    protected $_urlBuilder;
-  /** @var String */
-  private $apiKey;
+  /** @var \Twispay\Payments\Model\Config */
+  private $config;
 
   protected $_code = self::METHOD_CODE;
   protected $_isGateway = true;
@@ -39,11 +40,8 @@ class Twispay extends \Magento\Payment\Model\Method\AbstractMethod {
 
   /**
    * Constructor
-   * @param \Magento\Framework\App\RequestInterface $request
-   * @param \Magento\Framework\UrlInterface $urlBuilder
    * @param \Twispay\Payments\Helper\Payment $helper
-   * @param \Magento\Store\Model\StoreManagerInterface $storeManager
-   * @param \Magento\Framework\Locale\ResolverInterface $resolver
+   * @param \Twispay\Payments\Logger\Logger $twispayLogger
    * @param \Magento\Framework\Model\Context $context
    * @param \Magento\Framework\Registry $registry
    * @param \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory
@@ -51,18 +49,14 @@ class Twispay extends \Magento\Payment\Model\Method\AbstractMethod {
    * @param \Magento\Payment\Helper\Data $paymentData
    * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
    * @param \Magento\Payment\Model\Method\Logger $logger
-   * @param \Magento\Checkout\Model\Session $session
-   * @param \Twispay\Payments\Logger\Logger $twispayLogger
    * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource
    * @param \Magento\Framework\Data\Collection\AbstractDb|null $resourceCollection
    * @param array $data
    * @SuppressWarnings(PHPMD.ExcessiveParameterList)
    */
-  public function __construct( \Magento\Framework\App\RequestInterface $request
-                             , \Magento\Framework\UrlInterface $urlBuilder
-                             , \Twispay\Payments\Helper\Payment $helper
-                             , \Magento\Store\Model\StoreManagerInterface $storeManager
-                             , \Magento\Framework\Locale\ResolverInterface $resolver
+  public function __construct( \Twispay\Payments\Helper\Payment $helper
+                             , \Twispay\Payments\Logger\Logger $twispayLogger
+                             , \Twispay\Payments\Model\Config $config
                              , \Magento\Framework\Model\Context $context
                              , \Magento\Framework\Registry $registry
                              , \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory
@@ -70,18 +64,14 @@ class Twispay extends \Magento\Payment\Model\Method\AbstractMethod {
                              , \Magento\Payment\Helper\Data $paymentData
                              , \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
                              , \Magento\Payment\Model\Method\Logger $logger
-                             , \Magento\Checkout\Model\Session $session
-                             , \Twispay\Payments\Logger\Logger $twispayLogger
                              , \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null
                              , \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null
                              , array $data = [] )
   {
     parent::__construct($context, $registry, $extensionFactory, $customAttributeFactory, $paymentData, $scopeConfig, $logger, $resource, $resourceCollection, $data);
-    /* Initialize the logger. */
+    $this->helper = $helper;
     $this->log = $twispayLogger;
-    $this->_urlBuilder = $urlBuilder;
-
-    $this->apikey = $this->getApiKey();
+    $this->config = $config;
   }
 
 
@@ -128,7 +118,7 @@ class Twispay extends \Magento\Payment\Model\Method\AbstractMethod {
    *
    */
   public function authorize(\Magento\Payment\Model\InfoInterface $payment, $amount) {
-    $this->log->info(__FUNCTION__ . __(' Authorize payment'));
+    $this->log->info(__FUNCTION__ . __(': Authorize payment'));
     return $this;
   }
 
@@ -146,7 +136,7 @@ class Twispay extends \Magento\Payment\Model\Method\AbstractMethod {
    *
    */
   public function capture(\Magento\Payment\Model\InfoInterface $payment, $amount) {
-    $this->log->info(__FUNCTION__ . __(' Authorize and capture payment'));
+    $this->log->info(__FUNCTION__ . __(': Authorize and capture payment'));
     return $this;
   }
 
@@ -157,7 +147,81 @@ class Twispay extends \Magento\Payment\Model\Method\AbstractMethod {
    *
    */
   public function refund(\Magento\Payment\Model\InfoInterface $payment, $amount) {
-    $this->log->info(__FUNCTION__ . __(' Refund payment'));
+    $this->log->info(__FUNCTION__ . __(': Refund payment', $amount));
+
+    /* Get the order. */
+    $order = $payment->getOrder();
+    /* Extract the parent transaction. */
+    $parentTransaction = $this->helper->getTransaction($order->getId(), $this->_getParentTransactionId($payment));
+
+    /* Extract the transaction and transaction data. */
+    $parentTransactionData = $parentTransaction->getAdditionalInformation()['raw_details_info'];
+
+    /* Get the config values. */
+    $apiKey = $this->config->getApiKey();
+    $url = $this->config->getApiUrl();
+    if (('' == $apiKey) || ('' == $url)) {
+      /* Extract the message. */
+      $message = __(' Refund failed: Incomplete or missing configuration');
+      /* Log the error. */
+      $this->log->error(__FUNCTION__ . ':' . $message);
+
+      throw new \Magento\Framework\Validator\Exception($message);
+    }
+
+    /* Create the URL. */
+    $url = $url . '/transaction/' . $parentTransactionData['transactionId'];
+
+    /* Create the DELETE data arguments. */
+    $postData = 'amount=' . $amount . '&' . 'message=' . 'Refund for order ' . $parentTransactionData['orderId'];
+    $this->log->info(__('Refund amount=%1 for transaction=%2 from order %3', $amount, $parentTransactionData['transactionId'], $parentTransactionData['orderId']));
+    
+    /* Make the server request. */
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['accept: application/json', 'Authorization: ' . $apiKey]);
+    curl_setopt($ch, CURLOPT_POST, strlen($postData));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+
+    /* Send the request. */
+    $response = curl_exec($ch);
+    curl_close($ch);
+    /* Decode the response. */
+    $response = json_decode($response);
+
+    /* Check if the decryption was successful, the response code is 200 and message is 'Success'. */
+    if ((NULL !== $response) && (200 == $response->code) && ('Success' == $response->message)) {
+      /* Create a refund transaction */
+      $payment->setTransactionId($response->data->transactionId);
+      // $payment->addTransaction(Transaction::TYPE_REFUND, null, false, 'OK');
+      $payment->setTransactionAdditionalInfo( Transaction::RAW_DETAILS
+                                            , [ 'orderId'               => $parentTransactionData['orderId']
+                                              , 'refundedTransactionId' => $parentTransactionData['transactionId']
+                                              , 'transactionId'         => $response->data->transactionId
+                                              , 'amount'                => $amount]);
+      $payment->setIsTransactionClosed(TRUE);
+    } else {
+      /* Extract the message. */
+      $message = __(' Refund failed: Server returned error: %1', $response->code);
+      /* Log the error. */
+      $this->log->error(__FUNCTION__ . $message);
+
+      throw new \Magento\Framework\Validator\Exception($message);
+    }
+
     return $this;
+  }
+
+
+  /**
+   * Parent transaction id getter
+   *
+   * @param \Magento\Payment\Model\InfoInterface $payment
+   * @return string
+   */
+  protected function _getParentTransactionId(\Magento\Payment\Model\InfoInterface $payment) {
+    return $payment->getParentTransactionId() ? $payment->getParentTransactionId() : $payment->getLastTransId();
   }
 }
